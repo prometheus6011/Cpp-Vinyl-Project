baba218309a759542106c06bf5a2b130c23253cd
Mahi Nuthanapati <mahidhar1287@gmail.com>
2024-10-22 18:31:44 -0400

    .

--- mic_capture.cpp
+++ mic_capture.cpp
@@ -1,6 +1,30 @@
 #include <alsa/asoundlib.h>
+#include <SDL2/SDL.h>
 #include <libusb-1.0/libusb.h>
 #include <iostream>
+#include <vector>
+
+const int WINDOW_WIDTH = 800 ;
+const int WINDOW_HEIGHT = 400 ;
+
+void draw_waveform(SDL_Renderer* renderer, std::vector<short> & buffer)
+{
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255) ;
+	SDL_RenderClear(renderer) ;
+
+	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255) ;
+	int middle = WINDOW_HEIGHT / 2 ;
+	for (size_t i = 0; i < buffer.size() - 1; ++i) {
+		int x1 = (i * WINDOW_WIDTH) / buffer.size() ;
+		int y1 = middle - (buffer[i] * middle) / 32768 ;
+		int x2 = ((i + 1) * WINDOW_WIDTH) / buffer.size() ;
+		int y2 = middle - (buffer[i + 1] * middle) / 32768 ;
+
+		SDL_RenderDrawLine(renderer, x1, y1, x2, y2) ;
+	}
+
+	SDL_RenderPresent(renderer) ;
+}
 
 int main()
 {
@@ -28,15 +52,59 @@ int main()
 
 	short buffer[1024] ;
 	int frames ;
-	while (1) {
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		std::cerr << "SDL could not initialize! SDL Error: " << SDL_GetError() << std::endl ;
+		return EXIT_FAILURE ;
+	}
+
+	SDL_Window* window = SDL_CreateWindow("Mic Input Visualization", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN) ;
+
+	if (window == nullptr) {
+		std::cerr << "Window could not be opened! SDL Error: " << SDL_GetError() << std::endl ;
+		return EXIT_FAILURE ;
+	}
+
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED) ;
+	if (renderer == nullptr) {
+		std::cerr << "Renderer could not be created. SDL Error: " << SDL_GetError() << std::endl ;
+		return EXIT_FAILURE ;
+	}
+
+	std::vector<short> waveform_buffer(1024) ;
+	bool quit = false ;
+	SDL_Event e ;
+
+	while (!quit) {
+
+		while (SDL_PollEvent(&e) != 0) {
+			if (e.type == SDL_QUIT) {
+				quit = true ;
+			} else if (e.type == SDL_KEYDOWN) {
+				if (e.key.keysym.sym = SDLK_ESCAPE) {
+					quit = true ;
+				}
+			}
+
+		}
+
 		frames = snd_pcm_readi(handle, buffer, 1024) ;
 		if (frames < 0) {
 			std::cerr << "Error reading PCM device: " << snd_strerror(frames) << std::endl ;
 			break ;
 		}
+
+		waveform_buffer.assign(buffer, buffer + frames) ;
+
+		draw_waveform(renderer, waveform_buffer) ;
+
+		SDL_Delay(10) ;
 	}
 
 	snd_pcm_close(handle) ;
+	SDL_DestroyRenderer(renderer) ;
+	SDL_DestroyWindow(window) ;
+	SDL_Quit() ;
 
 	return EXIT_SUCCESS ;
 }
